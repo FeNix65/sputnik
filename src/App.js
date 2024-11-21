b7e5a7fa71c481637b43fade78d85bd5c4c1161a
FeNix65 <lykova-06@bk.ru>
2024-11-21 11:03:33 +0300

    aaa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { AppRoot } from '@telegram-apps/telegram-ui';
+import StartScreen from './pages/StartScreen';
 
 function App() {
+  if (window.Telegram?.WebApp) {
+    const tg = window.Telegram.WebApp;
+    tg.ready();
+
+    return (
+      <AppRoot>
+        <StartScreen />
+      </AppRoot>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ошибка: Telegram WebApp недоступен</h1>
     </div>
   );
+  
 }
 
 export default App;
---
+++ src/pages/StartScreen.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import '../assets/styles/StartScreen.css';
+import SputnikLogo from '../assets/images/sputnik-logo.png';
+import { Button } from '@telegram-apps/telegram-ui';
+
+function StartScreen() {
+  const tg = window.Telegram.WebApp;
+
+  // Подключаем тему
+  useEffect(() => {
+    const theme = tg?.themeParams || {};
+    document.documentElement.style.setProperty('--bg-color', theme.bg_color || '#FFFFFF');
+    document.documentElement.style.setProperty('--text-color', theme.text_color || '#000000');
+    document.documentElement.style.setProperty('--secondary-bg-color', theme.secondary_bg_color || '#F7F8FA');
+  }, [tg]);
+
+  return (
+    <div className="start-screen">
+      <div className="start-screen-content">
+        <h1>fdfdfdfd</h1>
+        <img
+          src={SputnikLogo}
+          alt="logo"
+          className="logo"
+        />
+        <h1>Спутник</h1>
+        <p>
+          Принципиально новый сервис для знакомств в Telegram, нацеленный на создание семейных пар.
+        </p>
+      </div>
+      <Button
+          size="l"
+          onClick={() => {
+            tg.sendData('profile:create'); // Отправка данных в Telegram WebApp
+          }}
+        >
+          Создать профиль
+        </Button>
+    </div>
+  );
+}
+
+export default StartScreen;
