51ba4a7a40b123bcc0b7653b75c500c6277855c9
Lida <lidiasiskina6@gmail.com>
2024-12-18 01:56:19 +0300

    взяла из async generalinf и habitation

--- src/pages/Habitation.js
+++ src/pages/Habitation.js
@@ -1,50 +1,73 @@
-import React, { useState } from 'react';
-import { Select, List, Section } from '@telegram-apps/telegram-ui';
+import React, { useState } from "react";
+import { Select, List, Section, Headline } from "@telegram-apps/telegram-ui";
 
 const Habitation = () => {
-    return (
-    {
-    render: () => <List style={{
-    
-    }}>
+  return {
+    render: () => (
+      <List style={{}}>
         <Section header="Проживание">
-          <Select placeholder="Где живете">
-            <option>Hello</option>
-            <option>Okay</option>
+          <Select placeholder="Где живете" name="Где живете" defaultValue="">
+            <option value="" disabled hidden>
+              Где живете
+            </option>
+            <option>Частный дом</option>
+            <option>Квартира</option>
+            <option>Убежище</option>
           </Select>
-        </Section>
 
-        <Section >
-          <Select placeholder="Финансовое положение">
-            <option>Hello</option>
-            <option>Okay</option>
+          <Select
+            placeholder="Финансовое положение"
+            name="Финансовое положение"
+            defaultValue=""
+          >
+            <option value="" disabled hidden>
+              Финансовое положение
+            </option>
+            <option>Финансовая свобода</option>
+            <option>Финансовая стабильность</option>
+            <option>Финансовая нестабильность</option>
+            <option>Финансовая яма</option>
           </Select>
-        </Section>
 
-        <Section >
-          <Select placeholder="Возможен ли переезд">
-            <option>Hello</option>
-            <option>Okay</option>
+          <Select
+            placeholder="Возможен ли переезд"
+            name="Возможен ли переезд"
+            defaultValue=""
+          >
+            <option value="" disabled hidden>
+              Возможен ли переезд
+            </option>
+            <option>Нет, живу не один</option>
+            <option>Да, живу один</option>
           </Select>
-        </Section>
 
-        <Section >
-          <Select placeholder="Наличие автомобиля">
-            <option>Hello</option>
-            <option>Okay</option>
+          <Select
+            placeholder="Наличие автомобиля"
+            name="Наличие автомобиля"
+            defaultValue=""
+          >
+            <option value="" disabled hidden>
+              Наличие автомобиля
+            </option>
+            <option>Есть</option>
+            <option>Отсутствует</option>
           </Select>
-        </Section>
 
-        <Section >
-          <Select placeholder="Наличие домашних животных">
-            <option>Hello</option>
-            <option>Okay</option>
+          <Select
+            placeholder="Наличие домашних животных"
+            name="Наличие домашних животных"
+            defaultValue=""
+          >
+            <option value="" disabled hidden>
+              Наличие домашних животных
+            </option>
+            <option>Есть</option>
+            <option>Отсутствует</option>
           </Select>
         </Section>
       </List>
-    } 
+    ),
+  };
+};
 
-
-    )};
-
-    export default Habitation;
+export default Habitation;
